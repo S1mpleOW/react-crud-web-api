f620710a9b0f9baf95a9becafb4aa7028db568e6
s1mpleow <dung.dq@geekup.vn>
2024-04-11 16:33:46 +0700

    Update PORT and REACT_APP_BASE_URL in .env and http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
-  headers: {
-    "Content-type": "application/json"
-  }
+	baseURL: process.env.REACT_APP_BASE_URL,
+	headers: {
+		'Content-type': 'application/json',
+	},
 });
